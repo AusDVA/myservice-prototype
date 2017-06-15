f5a754e4be8e9cdde9d3622be5362d2e0a30d05d
Alex Lee <zhuoyanlee@gmail.com>
2017-06-15 17:13:57 +1000

    Fixed some sass compilation issue by incorporating node-sass / gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,51 @@
-var gulp = require('gulp');  
-var nodemon = require('gulp-nodemon');  
-var sass = require('gulp-ruby-sass');  
-var autoprefixer = require('gulp-autoprefixer');  
-var jshint = require('gulp-jshint');  
-var livereload = require('gulp-livereload');  
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var jshint = require('gulp-jshint');
+var livereload = require('gulp-livereload');
 
-gulp.task('styles', function() {  
+gulp.task('sass', function () {
+  return gulp.src('src/sass/main.scss')
+    .pipe(sass({includePaths: ['src/sass']}))
+    .pipe(sass({outputStyle: 'compressed'})
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('docs/css'))
+    .pipe(livereload());
+});
+
+/*
+gulp.task('styles', function() {
   return sass('src/sass/*.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('docs/css'))
     .pipe(livereload());
 });
-
-gulp.task('scripts', function() {  
+*/
+gulp.task('scripts', function() {
   return gulp.src('docs/js/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(livereload());
 });
 
-gulp.task('ejs',function(){  
+gulp.task('ejs',function(){
     return gulp.src('views/**/*.ejs')
     .pipe(livereload());
 });
 
-gulp.task('watch', function() {  
+gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('src/sass/*.scss', ['styles']);
+    gulp.watch('src/sass/*.scss', ['sass']);
     gulp.watch('docs/js/*.js', ['scripts']);
     gulp.watch('views/**/*.ejs', ['ejs']);
 });
 
-gulp.task('server',function(){  
+gulp.task('server',function(){
     nodemon({
         'script': 'index.js',
         'ignore': 'docs/js/*.js'
     });
 });
 
-gulp.task('serve', ['server','watch']);  
+gulp.task('serve', ['server','watch']);
