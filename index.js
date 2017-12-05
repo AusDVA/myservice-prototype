b8e81fe478708e5c050a1640193c4ebb32e4419f
Millie Clery <milliebrewer@gmail.com>
2017-12-05 13:51:25 +1100

    no message

--- index.js
+++ index.js
@@ -299,13 +299,10 @@ app.get('/styleguide-icons', function(request, response) {
 app.get('/styleguide-callouts', function(request, response) {
   response.render('styleguide/pages/callouts');
 });
-<<<<<<< HEAD
 app.get('/styleguide-toast', function(request, response) {
   response.render('styleguide/pages/toast');
-=======
 app.get('/styleguide-alerts', function(request, response) {
   response.render('styleguide/pages/alerts');
->>>>>>> ac1a8e1f1fb2efc0aa47b2e7aa6d668f8f4f8295
 });
 app.get('/styleguide-globalmessages', function(request, response) {
   response.render('styleguide/pages/globalmessages');
