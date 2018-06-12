AusDVA/myservice-prototype

commit 687a293609902463883a1b437d42944543726f89

Minor update to mygov-linked animation timing

docs/js/students.js
@@ -222,7 +222,7 @@ jQuery(document).ready(function ($) {
         }
         if (!("claimantFlow" in localStorage)) {
           // if student 16 or 17 ask for TFN
-          if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
+          if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
             $(".pt-showIfStudentBetween16and18").show();
           } else {
             $(".pt-showIfStudentBetween16and18").hide();
@@ -247,7 +247,7 @@ jQuery(document).ready(function ($) {
 
         // if student 16 or 17 ask for TFN
         if (!("claimantFlow" in localStorage)) {
-          if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
+          if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
             $(".pt-showIfStudentBetween16and18").show();
           } else {
             $(".pt-showIfStudentBetween16and18").hide();
@@ -287,10 +287,10 @@ jQuery(document).ready(function ($) {
       // if a valid date
       if (dobDay && dobMonth && dobYear.length === 4) {
 
-        if (dobDay.length === 1) {
+        if (dobDay.longth === 1) {
           dobDay = "0" + dobDay;
         }
-        if (dobMonth.length === 1) {
+        if (dobMonth.longth === 1) {
           dobMonth = "0" + dobMonth;
         }
         var dob = dobYear + '-' + dobMonth + '-' + dobDay;
@@ -301,7 +301,6 @@ jQuery(document).ready(function ($) {
 
         localStorage.removeItem('studentAge');
         localStorage.setItem('studentAge', age);
-        console.debug('student age: ', age);
 
         // validation:: older than 5
         if (localStorage.getItem('studentAge') < 5) {
@@ -463,7 +462,7 @@ jQuery(document).ready(function ($) {
 
     $('input[name=engagedInFullTimeEmployment]').change(function () {
 
-      if ($('input[name=engagedInFullTimeEmployment]:checked').val() === 'yes') { } else {
+      if ($('input[name=engagedInFullTimeEmployment]:checked').val() === 'yes') {} else {
         if (!("veteranFlow" in localStorage)) {
 
           $('.pt-showIfEngagedInFullTimeEmployment').show();
@@ -474,7 +473,7 @@ jQuery(document).ready(function ($) {
       }
     });
 
-    if (!("studentFlow" in localStorage)) { }
+    if (!("studentFlow" in localStorage)) {}
 
     $('input[name=studentDependantOnVeteran]').change(function () {
       if ($('input[name=studentDependantOnVeteran]:checked').val() === 'yes') {
@@ -506,7 +505,6 @@ jQuery(document).ready(function ($) {
         localStorage.setItem('studentHasTFN', true);
       } else {
         localStorage.removeItem('studentHasTFN');
-        localStorage.removeItem('tfn');
       }
     });
 
@@ -991,7 +989,7 @@ jQuery(document).ready(function ($) {
         localStorage.removeItem('studentLevelOfStudy');
         localStorage.setItem('studentLevelOfStudy', 'primary');
         $(".pagination").find('button').prop('disabled', false);
-      } else if (selected_option.startsWith('secondary')) {
+      } else if (selected_option === 'secondary') {
 
         $(".pt-noLongerEligible").hide();
         $(".pt-showIfTertiary").hide();
@@ -1003,7 +1001,7 @@ jQuery(document).ready(function ($) {
         localStorage.setItem('studentLevelOfStudy', 'secondary');
 
         $(".pagination").find('button').prop('disabled', false);
-      } else if (selected_option.startsWith('tertiary') || selected_option === 'apprenticeship') {
+      } else if (selected_option === 'tertiary' || selected_option === 'apprenticeship') {
         $(".pt-noLongerEligible").hide();
         $(".pt-showIfPrimary").hide();
         $(".pt-showIfSecondary").hide();
@@ -1083,7 +1081,7 @@ jQuery(document).ready(function ($) {
 
     // if student 16 or 17 ask for TFN
     if ("claimantFlow" in localStorage || "veteranFlow" in localStorage) {
-      if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
+      if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
         $(".pt-showIfStudentBetween16and18").show();
       } else {
         $(".pt-showIfStudentBetween16and18").hide();
@@ -1136,7 +1134,6 @@ jQuery(document).ready(function ($) {
         $(".pt-showIfStudentTFN").show();
       } else {
         localStorage.setItem('studentTFN', false);
-        localStorage.removeItem('tfn');
         $(".pt-showIfNoStudentTFN").show('fast');
         $(".pt-showIfStudentTFN").hide();
       }
@@ -1158,12 +1155,12 @@ jQuery(document).ready(function ($) {
     }
 
     var businessDays = 5,
-      counter = 1; // set to 1 to count from next business day
+        counter = 1; // set to 1 to count from next business day
     while (businessDays > 0) {
       var tmp = new Date();
       tmp.setDate(tmp.getDate() + counter++);
       switch (tmp.getDay()) {
-        case 0: case 6:
+        case 0:case 6:
           break; // sunday & saturday
         default:
           businessDays--;
@@ -1316,11 +1313,11 @@ jQuery(document).ready(function ($) {
   // PoC file upload for prototype
   // TODO:: handle cancel 
   // TODO:: add additional items 
-  ; (function (document, window, index) {
+  ;(function (document, window, index) {
     var inputs = document.querySelectorAll('.file-upload__input');
     Array.prototype.forEach.call(inputs, function (input) {
       var label = input.nextElementSibling,
-        labelVal = label.innerHTML;
+          labelVal = label.innerHTML;
 
       input.addEventListener('change', function (e) {
         var fileName = '';
@@ -1359,14 +1356,5 @@ jQuery(document).ready(function ($) {
 
     $(this).toggleClass("accordion__trigger--open accordion__trigger--closed");
     $(this).closest(".accordion").toggleClass("accordion--closed accordion--open");
-  });
-
-  $('input[name=tax-file-number]').change(function () {
-    if (this.value) {
-      console.log('TFN set to: ', this.value);
-      localStorage.setItem('tfn', this.value);
-    } else {
-      localStorage.removeItem('tfn');
-    }
   });
 });

views/auth/mygov-linked.ejs
@@ -57,7 +57,7 @@
 				setTimeout(() => {
 					document.querySelector('.animation-start').classList.remove('transition-content--open');
 					document.querySelector('.animation-end').classList.add('transition-content--open');
-				}, 3500);
+				}, 2800);
 			</script>
 </body>
 

commit 948a3ced04d622efdb7a9b683571bcfe8132d6cb

MSY-2215 Withdraw claim - fix problem
* Commit https://github.com/AusDVA/myservice-prototype/commit/687a293609902463883a1b437d42944543726f89
broke the functionality of this ticket.  Fixed.

docs/js/students.js
@@ -222,7 +222,7 @@ jQuery(document).ready(function ($) {
         }
         if (!("claimantFlow" in localStorage)) {
           // if student 16 or 17 ask for TFN
-          if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
+          if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
             $(".pt-showIfStudentBetween16and18").show();
           } else {
             $(".pt-showIfStudentBetween16and18").hide();
@@ -247,7 +247,7 @@ jQuery(document).ready(function ($) {
 
         // if student 16 or 17 ask for TFN
         if (!("claimantFlow" in localStorage)) {
-          if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
+          if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
             $(".pt-showIfStudentBetween16and18").show();
           } else {
             $(".pt-showIfStudentBetween16and18").hide();
@@ -287,10 +287,10 @@ jQuery(document).ready(function ($) {
       // if a valid date
       if (dobDay && dobMonth && dobYear.length === 4) {
 
-        if (dobDay.longth === 1) {
+        if (dobDay.length === 1) {
           dobDay = "0" + dobDay;
         }
-        if (dobMonth.longth === 1) {
+        if (dobMonth.length === 1) {
           dobMonth = "0" + dobMonth;
         }
         var dob = dobYear + '-' + dobMonth + '-' + dobDay;
@@ -301,6 +301,7 @@ jQuery(document).ready(function ($) {
 
         localStorage.removeItem('studentAge');
         localStorage.setItem('studentAge', age);
+        console.debug('student age: ', age);
 
         // validation:: older than 5
         if (localStorage.getItem('studentAge') < 5) {
@@ -462,7 +463,7 @@ jQuery(document).ready(function ($) {
 
     $('input[name=engagedInFullTimeEmployment]').change(function () {
 
-      if ($('input[name=engagedInFullTimeEmployment]:checked').val() === 'yes') {} else {
+      if ($('input[name=engagedInFullTimeEmployment]:checked').val() === 'yes') { } else {
         if (!("veteranFlow" in localStorage)) {
 
           $('.pt-showIfEngagedInFullTimeEmployment').show();
@@ -473,7 +474,7 @@ jQuery(document).ready(function ($) {
       }
     });
 
-    if (!("studentFlow" in localStorage)) {}
+    if (!("studentFlow" in localStorage)) { }
 
     $('input[name=studentDependantOnVeteran]').change(function () {
       if ($('input[name=studentDependantOnVeteran]:checked').val() === 'yes') {
@@ -505,6 +506,7 @@ jQuery(document).ready(function ($) {
         localStorage.setItem('studentHasTFN', true);
       } else {
         localStorage.removeItem('studentHasTFN');
+        localStorage.removeItem('tfn');
       }
     });
 
@@ -989,7 +991,7 @@ jQuery(document).ready(function ($) {
         localStorage.removeItem('studentLevelOfStudy');
         localStorage.setItem('studentLevelOfStudy', 'primary');
         $(".pagination").find('button').prop('disabled', false);
-      } else if (selected_option === 'secondary') {
+      } else if (selected_option.startsWith('secondary')) {
 
         $(".pt-noLongerEligible").hide();
         $(".pt-showIfTertiary").hide();
@@ -1001,7 +1003,7 @@ jQuery(document).ready(function ($) {
         localStorage.setItem('studentLevelOfStudy', 'secondary');
 
         $(".pagination").find('button').prop('disabled', false);
-      } else if (selected_option === 'tertiary' || selected_option === 'apprenticeship') {
+      } else if (selected_option.startsWith('tertiary') || selected_option === 'apprenticeship') {
         $(".pt-noLongerEligible").hide();
         $(".pt-showIfPrimary").hide();
         $(".pt-showIfSecondary").hide();
@@ -1081,7 +1083,7 @@ jQuery(document).ready(function ($) {
 
     // if student 16 or 17 ask for TFN
     if ("claimantFlow" in localStorage || "veteranFlow" in localStorage) {
-      if (localStorage.getItem('studentAge') < 18 && localStorage.getItem('studentAge') > 15) {
+      if (localStorage.getItem('studentAge') <= 18 && localStorage.getItem('studentAge') > 15) {
         $(".pt-showIfStudentBetween16and18").show();
       } else {
         $(".pt-showIfStudentBetween16and18").hide();
@@ -1134,6 +1136,7 @@ jQuery(document).ready(function ($) {
         $(".pt-showIfStudentTFN").show();
       } else {
         localStorage.setItem('studentTFN', false);
+        localStorage.removeItem('tfn');
         $(".pt-showIfNoStudentTFN").show('fast');
         $(".pt-showIfStudentTFN").hide();
       }
@@ -1155,12 +1158,12 @@ jQuery(document).ready(function ($) {
     }
 
     var businessDays = 5,
-        counter = 1; // set to 1 to count from next business day
+      counter = 1; // set to 1 to count from next business day
     while (businessDays > 0) {
       var tmp = new Date();
       tmp.setDate(tmp.getDate() + counter++);
       switch (tmp.getDay()) {
-        case 0:case 6:
+        case 0: case 6:
           break; // sunday & saturday
         default:
           businessDays--;
@@ -1313,11 +1316,11 @@ jQuery(document).ready(function ($) {
   // PoC file upload for prototype
   // TODO:: handle cancel 
   // TODO:: add additional items 
-  ;(function (document, window, index) {
+  ; (function (document, window, index) {
     var inputs = document.querySelectorAll('.file-upload__input');
     Array.prototype.forEach.call(inputs, function (input) {
       var label = input.nextElementSibling,
-          labelVal = label.innerHTML;
+        labelVal = label.innerHTML;
 
       input.addEventListener('change', function (e) {
         var fileName = '';
@@ -1356,5 +1359,14 @@ jQuery(document).ready(function ($) {
 
     $(this).toggleClass("accordion__trigger--open accordion__trigger--closed");
     $(this).closest(".accordion").toggleClass("accordion--closed accordion--open");
+  });
+
+  $('input[name=tax-file-number]').change(function () {
+    if (this.value) {
+      console.log('TFN set to: ', this.value);
+      localStorage.setItem('tfn', this.value);
+    } else {
+      localStorage.removeItem('tfn');
+    }
   });
 });