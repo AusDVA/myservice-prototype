db99ec6ec591217486762bc5d09001095eb9d0e8
treborsantiago <44389375+treborsantiago@users.noreply.github.com>
2019-02-04 15:48:14 +1100

    Update

--- docs/js/income-assets.js
+++ docs/js/income-assets.js
@@ -8,9 +8,10 @@ function scrollToAdd() {
     }, 1000);
 }
 
-/* Clicks on "add" on title */
+/* Clicks on "add" on tile */
 
 function showNewCard(cardType) {
+    captureUserList();
     hideAll();
     myDiv = "#" + cardType + "-start";
     $(myDiv).show();
@@ -21,12 +22,11 @@ function showNewCard(cardType) {
 function addSummary(cardType) {
     hideAll();
     myDiv = "#" + cardType + "-summary";
-    $(myDiv).show();
+    userEntries.push($(myDiv));
+    displayUserList();
     $('html, body').animate({
         scrollTop: $("#user-list").offset().top
     }, 1000);
-    captureUserList();
-    updateUserList();
 };
 
 /* Clicks on "edit" on Summary Card */
@@ -43,11 +43,11 @@ function editCard(cardType) {
 function removeSummary(cardType) {
     hideAll();
     myDiv = "#" + cardType + "-summary";
-    $(myDiv).hide();
+    userEntries.splice($.inArray(myDiv, userEntries), 1);
+    displayUserList();
     $('html, body').animate({
         scrollTop: $("#user-list").offset().top
     }, 1000);
-    $("#add-options").show();
 };
 
 /* Clicks on "cancel" on full card */
@@ -57,22 +57,22 @@ function cancelCard(cardType) {
     $('html, body').animate({
         scrollTop: $("#user-list").offset().top
     }, 1000);
-    updateUserList();
+    displayUserList();
 }
 
 /* Update list after interaction */
 
-var currentUserList;
+var userEntries = [];
 
 function captureUserList() {
-    currentUserList = $("[id*=summary]:visible");
+    userEntries = $("[id*=summary]:visible");
 }
 
-function updateUserList() {
-    $.each(currentUserList, function (index, value) {
+function displayUserList() {
+    $.each(userEntries, function (index, value) {
         $(value).show();
     });
-    if (currentUserList.length > 0) {
+    if (userEntries.length > 0) {
         $("#add-anchor-link-start").hide();
         $("#add-anchor-link-return").show();
     } else {
@@ -85,10 +85,16 @@ function updateUserList() {
 /* Generic hide all */
 
 function hideAll() {
+
     $("#bank-accounts-start").hide();
     $("#bank-accounts-summary").hide();
     $("#bank-accounts-edit").hide();
     $("#church-account-ref").hide();
+
+    $("#cash-held-start").hide();
+    $("#cash-held-summary").hide();
+    $("#cash-held-edit").hide();
+
     $("#add-options").hide();
     $("#add-anchor-link-start").hide();
     $("#add-anchor-link-return").hide();
--- src/js/income-assets.js
+++ src/js/income-assets.js
@@ -1,93 +1,99 @@
-  /* On page scrolling */
+/* On page scrolling */
 
-  function scrollToAdd(){
+function scrollToAdd() {
     $('html, body').animate({
-          scrollTop: $("#add-options").offset().top
-      }, 1000);
-  }
+        scrollTop: $("#add-options").offset().top
+    }, 1000);
+}
 
-  /* Clicks on "add" on title */
+/* Clicks on "add" on tile */
 
-  function showNewCard(cardType) {
-      hideAll();
-      myDiv = "#" + cardType + "-start";
-      $(myDiv).show();
-  }
+function showNewCard(cardType) {
+    captureUserList();
+    hideAll();
+    myDiv = "#" + cardType + "-start";
+    $(myDiv).show();
+}
 
-  /* Clicks on "add" on full card */
+/* Clicks on "add" on full card */
 
-  function addSummary(cardType) {
-      hideAll();
-      myDiv = "#" + cardType + "-summary";
-      $(myDiv).show();
-      $('html, body').animate({
-          scrollTop: $("#user-list").offset().top
-      }, 1000);
-      captureUserList();
-      updateUserList() 
-  };
+function addSummary(cardType) {
+    hideAll();
+    myDiv = "#" + cardType + "-summary";
+    userEntries.push($(myDiv));
+    displayUserList();
+    $('html, body').animate({
+        scrollTop: $("#user-list").offset().top
+    }, 1000);
+};
 
-  /* Clicks on "edit" on Summary Card */
+/* Clicks on "edit" on Summary Card */
 
-  function editCard(cardType) {
-      captureUserList();
-      hideAll();
-      myDiv = "#" + cardType + "-edit";
-      $(myDiv).show();
-  }
+function editCard(cardType) {
+    captureUserList();
+    hideAll();
+    myDiv = "#" + cardType + "-edit";
+    $(myDiv).show();
+}
 
-  /* Clicks on "delete" on full card */
+/* Clicks on "delete" on full card */
 
-  function removeSummary(cardType) {
-      hideAll();
-      myDiv = "#" + cardType + "-summary";
-      $(myDiv).hide();
-      $('html, body').animate({
-          scrollTop: $("#user-list").offset().top
-      }, 1000);
-      $("#add-options").show();
-  };
+function removeSummary(cardType) {
+    hideAll();
+    myDiv = "#" + cardType + "-summary";
+    userEntries.splice($.inArray(myDiv, userEntries), 1);
+    displayUserList();
+    $('html, body').animate({
+        scrollTop: $("#user-list").offset().top
+    }, 1000);
+};
 
-  /* Clicks on "cancel" on full card */
+/* Clicks on "cancel" on full card */
 
-  function cancelCard(cardType) {
-      hideAll();
-      $('html, body').animate({
-          scrollTop: $("#user-list").offset().top
-      }, 1000);
-      updateUserList();
-  }
+function cancelCard(cardType) {
+    hideAll();
+    $('html, body').animate({
+        scrollTop: $("#user-list").offset().top
+    }, 1000);
+    displayUserList();
+}
 
-  /* Update list after interaction */
+/* Update list after interaction */
 
-  var currentUserList;
+var userEntries = [];
 
-  function captureUserList() {
-      currentUserList = $("[id*=summary]:visible");
-  }
+function captureUserList() {
+    userEntries = $("[id*=summary]:visible");
+}
 
-  function updateUserList() {
-      $.each(currentUserList, function(index, value) {
-          $(value).show();
-      });
-      if (currentUserList.length > 0) {
-          $("#add-anchor-link-start").hide();
-          $("#add-anchor-link-return").show();
-      } else {
-          $("#add-anchor-link-start").show();
-          $("#add-anchor-link-return").hide();
-      }
-      $("#add-options").show();
-  }
+function displayUserList() {
+    $.each(userEntries, function(index, value) {
+        $(value).show();
+    });
+    if (userEntries.length > 0) {
+        $("#add-anchor-link-start").hide();
+        $("#add-anchor-link-return").show();
+    } else {
+        $("#add-anchor-link-start").show();
+        $("#add-anchor-link-return").hide();
+    }
+    $("#add-options").show();
+}
 
-  /* Generic hide all */
+/* Generic hide all */
 
-  function hideAll() {
-      $("#bank-accounts-start").hide();
-      $("#bank-accounts-summary").hide();
-      $("#bank-accounts-edit").hide();
-      $("#church-account-ref").hide();
-      $("#add-options").hide();
-      $("#add-anchor-link-start").hide();
-      $("#add-anchor-link-return").hide();
-  }
+function hideAll() {
+
+    $("#bank-accounts-start").hide();
+    $("#bank-accounts-summary").hide();
+    $("#bank-accounts-edit").hide();
+    $("#church-account-ref").hide();
+
+    $("#cash-held-start").hide();
+    $("#cash-held-summary").hide();
+    $("#cash-held-edit").hide();
+
+    $("#add-options").hide();
+    $("#add-anchor-link-start").hide();
+    $("#add-anchor-link-return").hide();
+}
