ec4d1d63165035f334af494cc9273cb2f360a42d
Luke Wilson <luke@sitrobotsit.com>
2019-04-17 12:23:21 +1000

    switch account on global user working

--- docs/js/main.js
+++ docs/js/main.js
@@ -255,108 +255,6 @@ jQuery(document).ready(function ($) {
     localStorage.setItem('claimType', claimType);
   }
 
-  // Nom rep pages
-  function initNomRep() {
-
-    var repFlow = getUrlParameter('repFlow');
-
-    if (repFlow) {
-
-      localStorage.setItem('repFlow', repFlow);
-    } else if ("repFlow" in localStorage) {} else {
-      localStorage.setItem('repFlow', 'none');
-    }
-
-    if (localStorage.getItem('repFlow') == 'both') {
-      // alert('asdf');
-      jQuery(".pt-first-time-no").show();
-      jQuery(".pt-rep-flow-representing-no").hide();
-      jQuery(".pt-rep-flow-represented-no").hide();
-      jQuery(".pt-rep-flow-none").hide();
-      jQuery(".pt-rep-flow-representing").show();
-      jQuery(".pt-rep-flow-represented").show();
-    } else if (localStorage.getItem('repFlow') == 'none') {
-      jQuery(".pt-first-time-no").show();
-      jQuery(".pt-rep-flow-representing").hide();
-      jQuery(".pt-rep-flow-represented").hide();
-      jQuery(".pt-rep-flow-none-no").hide();
-      jQuery(".pt-rep-flow-none").show();
-    } else if (localStorage.getItem('repFlow') == 'representing') {
-      jQuery(".pt-first-time-no").show();
-      jQuery(".pt-rep-flow-represented").hide();
-      jQuery(".pt-rep-flow-representing-no").hide();
-      jQuery(".pt-rep-flow-none").hide();
-      jQuery(".pt-rep-flow-representing").show();
-    } else if (localStorage.getItem('repFlow') == 'represented') {
-      jQuery(".pt-first-time-no").show();
-      jQuery(".pt-rep-flow-representing").hide();
-      jQuery(".pt-rep-flow-none").hide();
-      jQuery(".pt-rep-flow-represented").show();
-      jQuery(".pt-rep-flow-represented-no").hide();
-    } else if (localStorage.getItem('repFlow') == 'newbie') {
-      jQuery(".pt-first-time-no").hide();
-      jQuery(".pt-first-time").show();
-    }
-  }
-
-  initNomRep();
-
-  // banners
-  function initBanners() {
-
-    var bannerFlow = getUrlParameter('bannerFlow');
-
-    if (bannerFlow) {
-
-      localStorage.setItem('bannerFlow', bannerFlow);
-    } else if ("bannerFlow" in localStorage) {} else {
-      localStorage.setItem('bannerFlow', 'none');
-    }
-
-    if (localStorage.getItem('bannerFlow') == 'all') {
-      jQuery(".pt-banner-service").show();
-      jQuery(".pt-banner-nr-rep").show();
-    } else if (localStorage.getItem('bannerFlow') == 'none') {
-      jQuery(".pt-banner-service").hide();
-      jQuery(".pt-banner-nr-rep").hide();
-    } else if (localStorage.getItem('bannerFlow') == 'service') {
-      jQuery(".pt-banner-service").show();
-      jQuery(".pt-banner-nr-rep").hide();
-    } else if (localStorage.getItem('bannerFlow') == 'nr-rep') {
-      jQuery(".pt-banner-service").hide();
-      jQuery(".pt-banner-nr-rep").show();
-    }
-  }
-
-  initBanners();
-
-  // Switch account
-  function initSwitch() {
-
-    var switchFlow = getUrlParameter('switchFlow');
-    var switchId = getUrlParameter('switchId');
-
-    if (switchFlow) {
-      localStorage.setItem('switchFlow', switchFlow);
-      localStorage.setItem('switchId', switchId);
-      jQuery('.pt-managing-user ').slideDown('fast');
-    }
-
-    if (localStorage.getItem('switchFlow') == 'active') {
-      // jQuery('.switch-account-button').addClass("switch-account-button--current");
-      jQuery('.pt-managing-user ').show();
-    } else {
-      // jQuery('.switch-account-button').removeClass("switch-account-button--current");
-    }
-  }
-
-  initSwitch();
-
-  $("#returnProfile").click(function () {
-    jQuery('.pt-managing-user ').slideUp('fast');
-    localStorage.setItem('switchFlow', 'none');
-  });
-
   // Swap text on top panel accordion button
   var open = false;
   var initialButtonText = $(".accordion-toppanel-btn").html();
@@ -388,14 +286,190 @@ jQuery(document).ready(function ($) {
   });
 });
 
-$(document).on("click", function () {
-  $(".switch-account-box").addClass("switch-account-box--hide");
-});
+// Nom rep pages
+function initNomRep() {
+
+  var repFlow = getUrlParameter('repFlow');
+
+  if (repFlow) {
+
+    localStorage.setItem('repFlow', repFlow);
+  } else if ("repFlow" in localStorage) {} else {
+    localStorage.setItem('repFlow', 'none');
+  }
+
+  if (localStorage.getItem('repFlow') == 'both') {
+    jQuery(".pt-first-time-no").show();
+    jQuery(".pt-rep-flow-representing-no").hide();
+    jQuery(".pt-rep-flow-represented-no").hide();
+    jQuery(".pt-rep-flow-none").hide();
+    jQuery(".pt-rep-flow-representing").show();
+    jQuery(".pt-rep-flow-represented").show();
+  } else if (localStorage.getItem('repFlow') == 'none') {
+    jQuery(".pt-first-time-no").show();
+    jQuery(".pt-rep-flow-representing").hide();
+    jQuery(".pt-rep-flow-represented").hide();
+    jQuery(".pt-rep-flow-none-no").hide();
+    jQuery(".pt-rep-flow-none").show();
+  } else if (localStorage.getItem('repFlow') == 'representing') {
+    jQuery(".pt-first-time-no").show();
+    jQuery(".pt-rep-flow-represented").hide();
+    jQuery(".pt-rep-flow-representing-no").hide();
+    jQuery(".pt-rep-flow-none").hide();
+    jQuery(".pt-rep-flow-representing").show();
+  } else if (localStorage.getItem('repFlow') == 'represented') {
+    jQuery(".pt-first-time-no").show();
+    jQuery(".pt-rep-flow-representing").hide();
+    jQuery(".pt-rep-flow-none").hide();
+    jQuery(".pt-rep-flow-represented").show();
+    jQuery(".pt-rep-flow-represented-no").hide();
+  } else if (localStorage.getItem('repFlow') == 'newbie') {
+    jQuery(".pt-first-time-no").hide();
+    jQuery(".pt-first-time").show();
+  }
+}
+
+initNomRep();
+
+// banners
+function initBanners() {
+
+  var bannerFlow = getUrlParameter('bannerFlow');
+
+  if (bannerFlow) {
+
+    localStorage.setItem('bannerFlow', bannerFlow);
+  } else if ("bannerFlow" in localStorage) {} else {
+    localStorage.setItem('bannerFlow', 'none');
+  }
+
+  if (localStorage.getItem('bannerFlow') == 'all') {
+    jQuery(".pt-banner-service").show();
+    jQuery(".pt-banner-nr-rep").show();
+  } else if (localStorage.getItem('bannerFlow') == 'none') {
+    jQuery(".pt-banner-service").hide();
+    jQuery(".pt-banner-nr-rep").hide();
+  } else if (localStorage.getItem('bannerFlow') == 'service') {
+    jQuery(".pt-banner-service").show();
+    jQuery(".pt-banner-nr-rep").hide();
+  } else if (localStorage.getItem('bannerFlow') == 'nr-rep') {
+    jQuery(".pt-banner-service").hide();
+    jQuery(".pt-banner-nr-rep").show();
+  }
+}
+
+initBanners();
+
+// Switch account
+function initSwitch() {
+
+  var switchFlow = getUrlParameter('switchFlow');
+  var switchId = getUrlParameter('switchId');
+
+  if (switchFlow) {
+    localStorage.setItem('switchFlow', switchFlow);
+    localStorage.setItem('switchId', switchId);
+    jQuery('.pt-managing-user ').slideDown('fast');
+  }
+
+  if (localStorage.getItem('switchFlow') == 'active') {
+    jQuery('.pt-managing-user ').show();
+  } else {}
+  // jQuery('.switch-account-button').removeClass("switch-account-button--current");
+
+
+  // hide the return to profile banner 
+  $("#returnProfile").click(function () {
+    jQuery('.pt-managing-user').slideUp('fast');
+    localStorage.setItem('switchFlow', 'none');
+
+    // localStorage.setItem('person', JSON.stringify(data[index]));
+    // localStorage.setItem('switchId', 'none');
+
+    // localStorage.setItem('switchFlow', 'none');
+    writeUser();
+  });
+
+  // hide switch account overlay when clicking elsewhere on the page
+  $(document).on("click", function () {
+    $(".switch-account-box").addClass("switch-account-box--hide");
+  });
+}
+
+initSwitch();
+
+// returns age from date of birth string 
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || m === 0 && today.getDate() < birthDate.getDate()) {
+    age--;
+  }
+  return age;
+}
 
 // local storage a user 
+
+function writeUser() {
+
+  console.log('Writing user --- ');
+
+  var user = JSON.parse(localStorage.getItem('person'));
+
+  // count number of clients 
+  if (user.clients.length > 0) {
+    user.numberOfClients = user.clients.length;
+    $('.pt-switch-account').show();
+  } else {
+    user.numberOfClients = 'none';
+    $('.pt-switch-account').hide();
+  }
+
+  var userHtml = '';
+  var start = '<div class="pt-flex-grid"><div class="pt-col">';
+  var end = '</div></div>';
+  userHtml += start + 'Name </div><div class="pt-col">' + user.nameFull + end;
+  userHtml += start + 'Age </div><div class="pt-col">' + getAge(user.dob) + end;
+  userHtml += start + 'Is a veteran </div><div class="pt-col">' + user.veteran + end;
+  userHtml += start + 'Number of clients </div><div class="pt-col">' + user.numberOfClients + end;
+
+  user.picture = '<img class="pt-image-circle" src="' + user.picture + '">';
+
+  $('#userContainerId').html(userHtml);
+  $('.pt-current-user-name-picture').html(user.picture);
+  $('.pt-current-user-name-first').html(user.name.first);
+  $('.pt-current-user-name-full').html(user.nameFull);
+
+  var clientListHtml = '';
+
+  $.each(user.clients, function (key, client) {
+    client.nameFull = client.nameFirst + ' ' + client.nameLast;
+
+    clientListHtml += '<li><a href="/auth?switchFlow=active&switchId=' + client.id + '" class="switch-account-box__link"><strong>';
+    clientListHtml += client.nameFirst + ' ' + client.nameLast + '</strong>';
+    clientListHtml += ' (' + client.role + ')</a></li>';
+  });
+
+  console.log(user.clients);
+  if (user.clients.length > 0 && localStorage.getItem('switchFlow') == 'active') {
+
+    // console.log(user.name);
+    // console.log(user.name.first);
+    // console.log(user.clients[localStorage.getItem('switchId')]);
+
+    $('.pt-current-user-name-first').html(user.clients[localStorage.getItem('switchId')].nameFirst);
+    $('.pt-current-user-name-full').html(user.clients[localStorage.getItem('switchId')].nameFull);
+  }
+
+  console.log('user - CHANGE');
+
+  $('.pt-current-user-client-list').html(clientListHtml);
+}
+
 // to generate more users, go to www.json-generator.com and paste in the data from /docs/data/user.generator 
 // and paste in the generated users in to /docs/data/user.json
-
 if (typeof Storage !== "undefined") {
 
   // Pull in the json content 
@@ -406,77 +480,26 @@ if (typeof Storage !== "undefined") {
   }).done(function (data) {
 
     console.log('User data back');
-
     $.each(data, function (index, element) {
 
       // console.log(element);
 
     });
 
-    // Switch account
-    function writeUser() {
-      var user = JSON.parse(localStorage.getItem('person'));
-      console.log('writing user');
-      var userHtml = '';
-
-      // localStorage.setItem('switchId');
-      // localStorage.removeItem('switchId');
-      // localStorage.setItem('switchFlow', 'none');
-      // console.log(user);
-
-
-      if (user.reps.length > 0) {
-        user.numberOfClients = user.clients.length;
-        $('.pt-switch-account').show();
-      } else {
-        user.numberOfClients = 'none';
-        $('.pt-switch-account').hide();
-      }
-
-      var start = '<div class="pt-flex-grid"><div class="pt-col">';
-      var end = '</div></div>';
-      userHtml += start + 'Name </div><div class="pt-col">' + user.nameFull + end;
-      // userHtml += start + 'DOB </div><div class="pt-col">' + moment(user.dob).format('YYYY-MM-DD') + end;
-      // userHtml += start + 'DVA Number </div><div class="pt-col">' + user.dVAClientNumber + end;
-      userHtml += start + 'Is a veteran </div><div class="pt-col">' + user.veteran + end;
-      userHtml += start + 'Number of clients </div><div class="pt-col">' + user.numberOfClients + end;
-      // userHtml += start + 'Last payment</div><div class="pt-col">' + user.lastPayment + end;
-
-
-      user.picture = '<img class="pt-image-circle" src="' + user.picture + '">';
-
-      $('#userContainerId').html(userHtml);
-      $('.pt-current-user-name-picture').html(user.picture);
-      $('.pt-current-user-name-first').html(user.name.first);
-      $('.pt-current-user-name-full').html(user.nameFull);
-
-      var clientListHtml = '';
-
-      $.each(user.clients, function (key, client) {
-
-        clientListHtml += '<li><a href="/auth?switchFlow=active&switchId=' + client.id + '" class="switch-account-box__link"><strong>';
-        clientListHtml += client.nameFull + '</strong>';
-        clientListHtml += ' (' + client.role + ')</a></li>';
-      });
-
-      if (user.reps.length > 0 && localStorage.getItem('switchFlow') == 'active') {
-        $('.pt-current-user-name-first').html(user.clients[localStorage.getItem('switchId')].nameFull);
-        $('.pt-current-user-name-full').html(user.clients[localStorage.getItem('switchId')].nameFull);
-      }
-
-      console.log('user - CHANGE');
-
-      $('.pt-current-user-client-list').html(clientListHtml);
+    // set the default MyService user if no user exists
+    if (!localStorage.getItem('person')) {
+      localStorage.setItem('person', JSON.stringify(data[0]));
+      console.log('setting the default user');
     }
 
-    writeUser();
-
+    // populate the user dropdown list with users from the json
     var $userSelect = $('#user-drop-down');
     $userSelect.empty();
     $.each(data, function (key, value) {
       $userSelect.append('<option value=' + value._id + '>' + value.nameFull + '</option>');
     });
 
+    // switch the user 
     $userSelect.change(function () {
 
       var selectedId = this.value;
@@ -485,54 +508,27 @@ if (typeof Storage !== "undefined") {
 
         if (element._id === selectedId) {
           console.log('index = ' + index);
+
           localStorage.setItem('person', JSON.stringify(data[index]));
+          localStorage.setItem('switchId', 'none');
+          jQuery('.pt-managing-user').slideUp('fast');
+          localStorage.setItem('switchFlow', 'none');
           writeUser();
         }
       });
     });
 
-    //   $("#highest-rank").change(function () {
-    //     if (22 < this.value && this.value < 30) {
-    //       console.log(this);
-    //       console.log($("#highest-rank option:selected").text());
-    //       var content = $("#highest-rank option:selected").text();;
-
-
-    //       $("#rank-info-chosen").html(content);
-
-    //       $("#rank-info").show();
-    //     } else {
-    //       $("#rank-info").hide();
-    //     }
-    //   });
-    // });
-
-
-    // $.each(user, function (index, element) {
-
-    // if (element.army) {
-    //   armArmy = element.army;
-    // }
-    // if (element.navy) {
-    //   armNavy = element.navy;
-    // }
-    // if (element.airforce) {
-    //   armAirforce = element.airforce;
-    // }
-
-    // console.log(element);
-
-    // });
-
+    writeUser();
   });
 } else {
-  alert("Sorry, your browser does not support web storage...");
+  console.log("browser does not support web storage...");
 }
 
 $(document).keypress(function (e) {
+
   switch (e.which) {
-    case 96:
-      //tilda
+    case 126:
+      //tilda + shift
       $('.pt-choose-user').toggle();
       break;
     default:
