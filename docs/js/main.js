06be27569e351de6c95dbd4365f5f1d40dadaa7f
Luke Wilson <luke@sitrobotsit.com>
2019-05-15 15:22:08 +1000

    tooltip now has awareness of position in space

--- docs/js/main.js
+++ docs/js/main.js
@@ -158,10 +158,13 @@ jQuery(document).ready(function ($) {
         control.setAttribute(ARIA_EXPANDED_ATTR, true);
 
         var className = tabOriginalClassName + ' ' + TAB_CLASS + '--active';
-        if (content.clientHeight > getOffsetDocumentTop(tab)) {
+
+        if (content.clientHeight > content.scrollTop + content.getBoundingClientRect().top) {
+          console.log('adding tooltip__tab--bottom');
           tab.className = className + ' ' + TAB_CLASS + '--bottom';
-          content.style.top = control.offsetTop + tab.offsetTop + tab.offsetHeight + 'px';
+          content.style.top = control.offsetTop + tab.clientHeight + tab.offsetTop + tab.offsetHeight + 'px';
         } else {
+          console.log('adding tooltip__tab--top');
           tab.className = className + ' ' + TAB_CLASS + '--top';
           content.style.top = control.offsetTop - content.clientHeight + tab.offsetTop + 'px';
         }
