3da1ca7fcd0f6963b7d20eac291b390375920a5d
Ally <9024018+AllySummers@users.noreply.github.com>
2019-07-25 17:01:31 +1000

    added radio icons. todo: add documentation

--- helpers/generateCheckRadioIcons.js
+++ helpers/generateCheckRadioIcons.js
@@ -12,24 +12,27 @@ module.exports = box => {
        } = box;
 
 
-  let printIconBox = icons => {
-    let printedIcons = icons.map(icon => {
-      if (typeof icon === "object") {
-        return `
-          <i class="fa-3x ${icon.weight} ${icon.icon}"></i>
-        ` 
-      } else if (typeof icon === "string") {
-        return `
-          <i class="fa-3x fas ${icon}"></i>
-        ` 
-      }
-    })
-
-    return printedIcons.join("")
+  if (typeof icon === "object") {
+    icon = `<i class="fa-3x ${icon.weight} ${icon.icon}"></i>` ;
+  } else if (typeof icon === "string") {
+    icon = `<i class="fa-3x fas ${icon}"></i>`;
   }
 
   let html =  `
-
+  <div class="radio-icon">
+    <input type="${type}" name="${baseID}" id="${baseID}-${boxID}" class="radio-icon__control"
+      ${toggle ? `data-toggle="${toggle}"` : ""}
+      ${rToggle ? `data-r-toggle="${rToggle}"` : ""}
+      ${inject ? `data-inject="${inject}"` : ""}
+      ${modifiers && modifiers.includes("disabled") ? "disabled" : ""}
+      ${modifiers && modifiers.includes("hidden") ? "hidden" : ""}
+      ${modifiers && modifiers.includes("checked") ? "checked" : ""}
+    >
+    <label for="${baseID}-${boxID}" class="radio-icon__label">
+      ${icon}
+      <span class="radio-icon__text">${text}</span>
+    </label>
+  </div>
   <label class="mys-radio">
     <input type="${type}" name="${baseID}" id="${baseID}-${boxID}" class="mys-radio__control"
       ${toggle ? `data-toggle="${toggle}"` : ""}
@@ -39,7 +42,7 @@ module.exports = box => {
       ${modifiers && modifiers.includes("hidden") ? "hidden" : ""}
       ${modifiers && modifiers.includes("checked") ? "checked" : ""}>
     <span class="mys-radio__box mys-radio__box--large">
-      ${printIconBox(icons)}
+      ${icon}
       <div class="margin-above">${text}</div>
     </span>
   </label>`
