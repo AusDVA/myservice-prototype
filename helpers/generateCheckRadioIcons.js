6be85999903df2dcb16225dd5bd14b7bdf11aa6b
Ally <9024018+AllySummers@users.noreply.github.com>
2019-08-14 18:59:00 +1000

    lots of extra functionality in form partials - still need to document

--- helpers/generateCheckRadioIcons.js
+++ helpers/generateCheckRadioIcons.js
@@ -1,3 +1,5 @@
+const generateTooltip = require('./generateTooltip');
+
 module.exports = box => {
   let { modifiers, 
         text, 
@@ -8,11 +10,21 @@ module.exports = box => {
         type,
         baseID,
         boxID,
-        icon
+        icon,
+        suppliedID,
+        tooltip
        } = box;
 
-  let generateID = box.id ? box.id : baseID + '-' + boxID;
+  let generatedID = suppliedID ? boxID : `${baseID}-${boxID}`;
 
+  if (tooltip) {
+    tooltip = generateTooltip({
+      content: tooltip.content,
+      screenreaderText: tooltip.screenreaderText,
+      id: generatedID,
+      modifiers
+    })
+  }
 
   if (typeof icon === "object") {
     icon = `<i class="radio_icons__icon ${icon.weight ? icon.weight : 'fal'} ${icon.icon}"></i>` ;
@@ -22,17 +34,17 @@ module.exports = box => {
 
   let html =  `
   <div class="radio-icon">
-    <input type="${type}" name="${baseID}" id="${generateID}" class="radio-icon__control"
+    <input type="${type}" name="${baseID}" id="${generatedID}" class="radio-icon__control"
       ${toggle ? `data-toggle="${toggle}"` : ""}
       ${rToggle ? `data-r-toggle="${rToggle}"` : ""}
       ${inject ? `data-inject="${inject}"` : ""}
-      ${modifiers && modifiers.includes("disabled") ? "disabled" : ""}
-      ${modifiers && modifiers.includes("hidden") ? "hidden" : ""}
-      ${modifiers && modifiers.includes("checked") ? "checked" : ""}
+      ${modifiers.includes("disabled") ? "disabled" : ""}
+      ${modifiers.includes("hidden") ? "hidden" : ""}
+      ${modifiers.includes("checked") ? "checked" : ""}
     >
-    <label for="${generateID}" class="radio-icon__label">
+    <label for="${generatedID}" class="radio-icon__label">
       ${icon}
-      <span class="radio-icon__text">${text}</span>
+      <span class="radio-icon__text">${text} ${tooltip ? tooltip : ""}</span>
     </label>
   </div>`
 
