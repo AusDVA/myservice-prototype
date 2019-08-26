c1bebc8732274fac2b241ac2eaaa1fbde7d377f5
Matthew McInerney <mattwsmcinerney@gmail.com>
2019-08-27 00:23:34 +1000

    added install code

--- service-worker.js
+++ service-worker.js
@@ -100,31 +100,3 @@ self.addEventListener('fetch', function (e) {
       }) // end caches.match(e.request)
   ); // end e.respondWith
 });
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 76 and later from showing the mini-infobar
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  showInstallPromotion();
-});
-
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
