08b0ac33c0fe9966e026c57a3773fc1114166d8e
Ally <9024018+AllySummers@users.noreply.github.com>
2019-10-16 17:05:22 +1100

    start of new gulp build system

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 let express = require('express'),
   cookieParser = require("cookie-parser"),
   serveIndex = require('serve-index'),
-  {
-    promisify
-  } = require('util'),
-  {
-    resolve
-  } = require('path'),
   fs = require('fs'),
   path = require('path'),
-  // featuretoggleapi = require('feature-toggle-api'),
-
-  // not so secret secret
-  secret = 'eeeek',
+  { promisify } = require('util'),
+  { resolve } = require('path'),
 
   // will use the PORT environment variable if present,
   // else use first argument from command line for PORT,
@@ -34,14 +26,7 @@ app.get('/favicon.ico', (req, res) => {
   res.send();
 })
 
-// this is a pretty nice feature toggle. might be cool to do something like this in the future
-// https://www.npmjs.com/package/feature-toggle-api
-// var api = featuretoggleapi({
-//   feature1: false,
-//   feature2: true
-// });
-
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.partials = __dirname + '/partials/';
   res.locals.forms = __dirname + '/partials/components/form-partials/';
   res.locals.components = __dirname + '/partials/components/';
@@ -58,49 +43,24 @@ app.use(function (req, res, next) {
   next();
 });
 
-
-
-// create sitemap 
-app.use('/files', serveIndex('views', {
-  'icons': true
-}));
-
-app.use(function (req, res, next) {
-  res.locals.partials = __dirname + '/partials/';
-  next();
-});
-
-
 // create sitemap 
 app.use('/files', serveIndex('views', {
   'icons': true
 }));
 
 // using body parser to parse the body of incoming post requests
-app.use(require('body-parser').urlencoded({
-  extended: true // must give a value for extended
-}));
+app.use(require('body-parser').urlencoded({ extended: true }));
 
 // using express-session for session cookies
-app.use(
-
-  require('express-session')(
-
-    {
-      name: 'site_cookie',
-      secret: secret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-
-        // make session cookies only last 15 seconds
-        // for the sake of this demo
-        maxAge: 15000
-
-      }
-    }
-  )
-);
+app.use(require('express-session')({
+  name: 'site_cookie',
+  secret: 'eeeek',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 15000
+  }
+}));
 
 console.log('build env:', app.settings.env);
 var liveFeatureList = require('./feature-flag-list.json');
@@ -161,74 +121,73 @@ app.get('/sitemap', (req, res) => {
       })
     })
     .catch(e => console.error(e));
-
-
 });
 
 // folder level renders 
-app.get('/:id0', function (request, response) {
+app.get('/:id0', (request, response) => {
   response.render(request.params.id0, {
     main_nav_active: 'home',
     liveFeature: liveFeatureEnv
   });
 });
 
-app.get('/:id0/:id1', function (request, response) {
-  response.render(request.params.id0 + "/" + request.params.id1, {
-    main_nav_active: request.params.id1,
+app.get('/:id0/:id1', (request, response) => {
+  let { id0, id1 } = request.params;
+  response.render(`${id0}/${id1}`, {
+    main_nav_active: id1,
     liveFeature: liveFeatureEnv
   });
 });
 
-app.get('/:id0/:id1/:id2', function (request, response) {
-  response.render(request.params.id0 + "/" + request.params.id1 + "/" + request.params.id2, {
-    main_nav_active: request.params.id1,
+app.get('/:id0/:id1/:id2', (request, response) => {
+  let { id0, id1, id2 } = request.params;
+  response.render(`${id0}/${id1}/${id2}`, {
+    main_nav_active: id1,
     liveFeature: liveFeatureEnv,
-    secondary_nav_active: request.params.id2,
+    secondary_nav_active: id2,
     claimType: request.cookies.claimType
   });
 });
 
-app.get('/:id0/:id1/:id2/:id3', function (request, response) {
-  response.render(request.params.id0 + "/" + request.params.id1 + "/" + request.params.id2 + "/" + request.params.id3, {
-    main_nav_active: request.params.id1,
+app.get('/:id0/:id1/:id2/:id3', (request, response) => {
+  let { id0, id1, id2, id3 } = request.params;
+  response.render(`${id0}/${id1}/${id2}/${id3}`, {
+    main_nav_active: id1,
     liveFeature: liveFeatureEnv,
-    secondary_nav_active: request.params.id2
+    secondary_nav_active: id2
   });
 });
 
-app.get('/',
-  function (request, response) {
-    response.render('unauth/index-loading', {
-      layout: 'login',
-      user: request.user,
-      liveFeature: liveFeatureEnv
-    });
-  });
-
-app.get('/mygov-login',
-  function (request, response) {
-    response.render('auth/mygov-login', {
-      layout: 'login',
-      user: request.user,
-      liveFeature: liveFeatureEnv
-    });
-  });
-
-app.get('/logout',
-  function (request, response) {
-    request.logout();
-    response.redirect('/');
-  });
-
-app.post('/styleguide/new_forms_preview', (req, res) => {
-  let form_preview = req.body.form_preview;
-  console.log(form_preview);
-  res.render('styleguide/new_forms_preview', {
-    form_preview
+app.get('/:id0/:id1/:id2/:id3/id:4', (request, response) => {
+  let { id0, id1, id2, id3, id4 } = request.params;
+  response.render(`${id0}/${id1}/${id2}/${id3}/${id4}`, {
+    main_nav_active: id1,
+    liveFeature: liveFeatureEnv,
+    secondary_nav_active: id2
   });
 });
 
-app.listen(port, function () {
+app.get('/', (request, response) => {
+  response.render('unauth/index-loading', {
+    layout: 'login',
+    user: request.user,
+    liveFeature: liveFeatureEnv
+  });
+});
+
+app.get('/mygov-login', (request, response) => {
+  response.render('auth/mygov-login', {
+    layout: 'login',
+    user: request.user,
+    liveFeature: liveFeatureEnv
+  });
+});
+
+app.get('/logout', (request, response) => {
+  request.logout();
+  response.redirect('/');
+});
+
+app.listen(port, () => {
   console.log('listening on port: ' + port);
 });
