301275949643c9486e03b856bdffd2fe3312ca1d
Matthew McInerney <mattwsmcinerney@gmail.com>
2020-02-05 11:53:37 +1100

    changed originalUrl to path to resolve url query string conflicts

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let express = require('express'),
+let express = require("express"),
   cookieParser = require("cookie-parser"),
-  serveIndex = require('serve-index'),
-  fs = require('fs'),
-  path = require('path'),
-  { promisify } = require('util'),
-  { resolve } = require('path'),
-
+  serveIndex = require("serve-index"),
+  fs = require("fs"),
+  path = require("path"),
+  { promisify } = require("util"),
+  { resolve } = require("path"),
   // will use the PORT environment variable if present,
   // else use first argument from command line for PORT,
   // else default to a hard coded value of 4000
@@ -19,25 +18,25 @@ let stat = promisify(fs.stat);
 app.use(express.static(__dirname));
 app.use(cookieParser());
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-app.get('/favicon.ico', (req, res) => {
+app.get("/favicon.ico", (req, res) => {
   res.send();
-})
+});
 
-console.log('build env:', app.settings.env);
-var liveFeatureList = require('./feature-flag-list.json');
+console.log("build env:", app.settings.env);
+var liveFeatureList = require("./feature-flag-list.json");
 var liveFeatureEnv = [];
 
 // loading in different lists depending on which git branch (but not in heroku)
 if (app.settings.env === "development") {
-  var gitBranch = require('git-branch');
+  var gitBranch = require("git-branch");
 }
 
-if (typeof gitBranch !== 'undefined' && gitBranch) {
-  console.log('Working on branch:', gitBranch.sync());
-  if (gitBranch.sync() === 'master') {
+if (typeof gitBranch !== "undefined" && gitBranch) {
+  console.log("Working on branch:", gitBranch.sync());
+  if (gitBranch.sync() === "master") {
     liveFeatureList = liveFeatureList.production;
   } else {
     liveFeatureEnv = liveFeatureList.development;
@@ -48,52 +47,57 @@ if (typeof gitBranch !== 'undefined' && gitBranch) {
   liveFeatureEnv = liveFeatureList.development;
 }
 
-console.log('List of features that are unhidden:');
+console.log("List of features that are unhidden:");
 console.log(liveFeatureList);
 
 app.use((req, res, next) => {
-  res.locals.partials = __dirname + '/partials/';
-  res.locals.forms = __dirname + '/partials/components/form-partials/';
-  res.locals.components = __dirname + '/partials/components/';
-  res.locals.templates = __dirname + '/partials/templates/';
-  res.locals.content = __dirname + '/partials/content/';
-  res.locals.formPartialsID = require('./helpers/formPartialsID');
-  res.locals.generateOption = require('./helpers/generateOption');
-  res.locals.replaceNonAlphanumeric = require('./helpers/replaceNonAlphanumeric');
-  res.locals.generateCheckRadio = require('./helpers/generateCheckRadio');
-  res.locals.generateCheckRadioIcons = require('./helpers/generateCheckRadioIcons');
-  res.locals.generateTooltip = require('./helpers/generateTooltip');
-  res.locals.generateLabel = require('./helpers/generateLabel');
-  res.locals.generateButtonRadio = require('./helpers/generateButtonRadio');
-  res.locals.locals = { liveFeature: liveFeatureEnv }
+  res.locals.partials = __dirname + "/partials/";
+  res.locals.forms = __dirname + "/partials/components/form-partials/";
+  res.locals.components = __dirname + "/partials/components/";
+  res.locals.templates = __dirname + "/partials/templates/";
+  res.locals.content = __dirname + "/partials/content/";
+  res.locals.formPartialsID = require("./helpers/formPartialsID");
+  res.locals.generateOption = require("./helpers/generateOption");
+  res.locals.replaceNonAlphanumeric = require("./helpers/replaceNonAlphanumeric");
+  res.locals.generateCheckRadio = require("./helpers/generateCheckRadio");
+  res.locals.generateCheckRadioIcons = require("./helpers/generateCheckRadioIcons");
+  res.locals.generateTooltip = require("./helpers/generateTooltip");
+  res.locals.generateLabel = require("./helpers/generateLabel");
+  res.locals.generateButtonRadio = require("./helpers/generateButtonRadio");
+  res.locals.locals = { liveFeature: liveFeatureEnv };
   next();
 });
 
-// create sitemap 
-app.use('/files', serveIndex(__dirname, {
-  'icons': true
-}));
+// create sitemap
+app.use(
+  "/files",
+  serveIndex(__dirname, {
+    icons: true
+  })
+);
 
 // using body parser to parse the body of incoming post requests
-app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require("body-parser").urlencoded({ extended: true }));
 
 app.use(
-  require('express-session')({
-    name: 'site_cookie',
-    secret: 'eeeek',
+  require("express-session")({
+    name: "site_cookie",
+    secret: "eeeek",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 15000 }
   })
 );
 
-app.get('/sitemap', (req, res) => {
+app.get("/sitemap", (req, res) => {
   async function getFiles(dir) {
     const subdirs = await readdir(dir);
-    const files = await Promise.all(subdirs.map(async (subdir) => {
-      const res = resolve(dir, subdir);
-      return (await stat(res)).isDirectory() ? getFiles(res) : res;
-    }));
+    const files = await Promise.all(
+      subdirs.map(async subdir => {
+        const res = resolve(dir, subdir);
+        return (await stat(res)).isDirectory() ? getFiles(res) : res;
+      })
+    );
     return files.reduce((a, f) => a.concat(f), []);
   }
 
@@ -105,60 +109,80 @@ app.get('/sitemap', (req, res) => {
         file = file.replace(/\\/g, "/");
         file = file.replace("/views", "");
 
-        const data = fs.readFileSync(`./views/${file}`, "utf8")
+        const data = fs.readFileSync(`./views/${file}`, "utf8");
 
-        var title = data.match(/<%- include\(templates\+"header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/) || data.match(/<title>(.*)<\/title>/) || null;
-        var heading = data.match(/<%- include\(templates\+"header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/) || data.match(/<%- include\(components\+"styleguide\/styleguide-header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/) || null;
-        var claim = data.match(/<%- include\(templates\+"header"[\s\S]*?claim: "(.*?)"[\s\S]*?%>/) || data.match(/<%- include\(components\+"styleguide\/styleguide-header"[\s\S]*?claim: "(.*?)"[\s\S]*?%>/) || null;
+        var title =
+          data.match(
+            /<%- include\(templates\+"header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/
+          ) ||
+          data.match(/<title>(.*)<\/title>/) ||
+          null;
+        var heading =
+          data.match(
+            /<%- include\(templates\+"header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/
+          ) ||
+          data.match(
+            /<%- include\(components\+"styleguide\/styleguide-header"[\s\S]*?heading: "(.*?)"[\s\S]*?%>/
+          ) ||
+          null;
+        var claim =
+          data.match(
+            /<%- include\(templates\+"header"[\s\S]*?claim: "(.*?)"[\s\S]*?%>/
+          ) ||
+          data.match(
+            /<%- include\(components\+"styleguide\/styleguide-header"[\s\S]*?claim: "(.*?)"[\s\S]*?%>/
+          ) ||
+          null;
 
         if (title !== null) title = JSON.parse(JSON.stringify(title))[1];
         if (heading !== null) heading = JSON.parse(JSON.stringify(heading))[1];
-        if (claim !== null) claim = JSON.parse(JSON.stringify(claim))[1]
+        if (claim !== null) claim = JSON.parse(JSON.stringify(claim))[1];
 
         return {
           file,
           title,
           claim,
           heading
-        }
-      })
+        };
+      });
 
       const sortedClaims = [...new Set(allFiles.map(file => file.claim))]
-        .map(claim => allFiles
-          .filter(files => files.claim === claim
-        )).reverse();
+        .map(claim => allFiles.filter(files => files.claim === claim))
+        .reverse();
 
-      res.render('sitemap', {
+      res.render("sitemap", {
         sortedClaims
-      })
-    })
+      });
+    });
   })();
-})
+});
 
-app.get('/', (req, res) => {
-  res.render("unauth/index-loading")
-})
+app.get("/", (req, res) => {
+  res.render("unauth/index-loading");
+});
 
-app.get('*', (req, res) => {
-  var url = req.originalUrl.replace(".ejs", "");
+app.get("*", (req, res) => {
+  // var url = req.originalUrl.replace(".ejs", "");
+  var url = req.path;
 
   if (url.charAt(0) === "/") url = url.substr(1);
 
   var file = path.join(`${__dirname}/views/${url}.ejs`);
   var index = path.join(`${__dirname}/views/${url}/index.ejs`);
-  
+
   if (liveFeatureEnv === liveFeatureList.production) {
     if (fs.existsSync(file) || fs.existsSync(index)) {
-      res.render(url, { source_url: req.originalUrl })
+      // res.render(url, { source_url: req.originalUrl })
+      res.render(url, { source_url: req.path });
     } else {
-      res.render("unauth/not-found", { source_url: req.originalUrl })
+      // res.render("unauth/not-found", { source_url: req.originalUrl })
+      res.render("unauth/not-found", { source_url: req.path });
     }
   } else {
-    res.render(url, { source_url: req.originalUrl })
+    res.render(url, { source_url: req.originalUrl });
   }
-
-})
+});
 
 app.listen(port, () => {
-  console.log('listening on port: ' + port);
+  console.log("listening on port: " + port);
 });
