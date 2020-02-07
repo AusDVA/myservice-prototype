f48df8d1a37add58fc935fa925e4d49c35f72747
treborsantiago <44389375+treborsantiago@users.noreply.github.com>
2020-02-07 15:18:56 +1100

    added unlisted shares

--- src/js/income-assets-profile.js
+++ src/js/income-assets-profile.js
@@ -19,7 +19,6 @@ function showNewCard(cardType) {
   $(myDelete).hide();
   $(myForm).trigger("reset");
   $(myDiv).show();
-  // $("#continue").attr('disabled','disabled');
   $('html, body').animate({
     scrollTop: $("#user-list").offset().top
   }, 1000);
@@ -30,6 +29,7 @@ function showNewCard(cardType) {
 function addSummary(cardType) {
   hideAll();
   var myDiv = "#" + cardType + "-summary";
+  console.log( myDiv )
   userEntries.push($(myDiv));
   displayUserList();
   $('html, body').animate({
@@ -44,6 +44,7 @@ function editCard(cardType) {
   captureUserList();
   hideAll();
   var myDiv = "#" + cardType + "-card";
+  console.log( myDiv)
   var myDelete = myDiv + " #deleteButton";
   var myAdd = myDiv + " #addButton";
   $(myDelete).show();
@@ -101,7 +102,6 @@ function displayUserList() {
 
 function hideAll() {
 
-
   $("#user-list").hide();
   $("#main-pagination").hide();
 
@@ -113,97 +113,14 @@ function hideAll() {
 
   $("#bank-accounts-new-card").hide();
   $("#bank-accounts-new-summary").hide();
+
+  $("#shares-listed-update-card").hide();
+  $("#shares-listed-update-summary").hide();
   
+  // $("#church-account-ref").hide();
 
-  $("#church-account-ref").hide();
-
-  // $("#cash-held-card").hide();
-  // $("#cash-held-summary").hide();
-
-  // $("#superannuation-card").hide();
-  // $("#superannuation-summary").hide();
-
-  // $("#shares-card").hide();
-  // $("#shares-summary").hide();
-
-  // $("#managed-investments-card").hide();
-  // $("#managed-investments-summary").hide();
-
-  // $("#other-investments-card").hide();
-  // $("#other-investments-summary").hide();
-
-  // $("#money-loaned-card").hide();
-  // $("#money-loaned-summary").hide();
-
-  // $("#bonds-and-debentures-card").hide();
-  // $("#bonds-and-debentures-summary").hide();
-
-  // $("#other-payments-card").hide();
-  // $("#other-payments-summary").hide();
-
-  // $("#employment-income-card").hide();
-  // $("#employment-income-summary").hide();
-
-  // $("#past-employment-card").hide();
-  // $("#past-employment-summary").hide();
-
-  // $("#super-pension-card").hide();
-  // $("#super-pension-summary").hide();
-
-  // $("#foreign-pension-card").hide();
-  // $("#foreign-pension-summary").hide();
-
-  // $("#boarders-and-lodgers-card").hide();
-  // $("#boarders-and-lodgers-summary").hide();
-
-  // $("#purchased-income-streams-card").hide();
-  // $("#purchased-income-streams-summary").hide();
-
-  // $("#foreign-income-and-assets-card").hide();
-  // $("#foreign-income-and-assets-summary").hide();
-
-  // $("#real-estate-card").hide();
-  // $("#real-estate-summary").hide();
-
-  // $("#farm-card").hide();
-  // $("#farm-summary").hide();
-
-  // $("#self-managed-super-card").hide();
-  // $("#self-managed-super-summary").hide();
-
-  // $("#businesses-card").hide();
-  // $("#businesses-summary").hide();
-
-  // $("#home-contents-card").hide();
-  // $("#home-contents-summary").hide();
-
-  // $("#vehicles-card").hide();
-  // $("#vehicles-summary").hide();
-
-  // $("#other-personal-assets-card").hide();
-  // $("#other-personal-assets-summary").hide();
-
-  // $("#life-insurance-card").hide();
-  // $("#life-insurance-summary").hide();
-
-  // $("#gifts-card").hide();
-  // $("#gifts-summary").hide();
-
-  // $("#maintenance-paid-to-former-partner-card").hide();
-  // $("#maintenance-paid-to-former-partner-summary").hide();
-
-  // $("#addtional-other-personal-assets-card").hide();
-  // $("#addtional-other-personal-assets-summary").hide();
-
-  // $("#compensation-and-damages-card").hide();
-  // $("#compensation-and-damages-summary").hide();
-
-  // $("#add-options").hide();
-  $("#add-anchor-link-start").hide();
-  $("#add-anchor-link-return").hide();
-  // $("#footer-buttons-mask").hide();
-
-  // $("#continue").removeAttr('disabled');
+  // $("#add-anchor-link-start").hide();
+  // $("#add-anchor-link-return").hide();
 
 }
 
