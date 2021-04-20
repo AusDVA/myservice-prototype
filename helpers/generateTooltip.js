c98d758a8b00a180fd1571872365db4efb825777
Ben Schultz <benjamin.schultz@humanservices.gov.au>
2021-04-20 14:59:49 +0930

    Revert tooltips.

--- helpers/generateTooltip.js
+++ helpers/generateTooltip.js
@@ -4,20 +4,20 @@ module.exports = tooltip => {
 
 	// if (!screenreaderText || !content || !id) return "All fields mandatory";
 
-	return `
-    <div class="tooltip">
-      <div id="${id}__label" class="tooltip__control" role="button" tabindex="0" aria-describedby="${id}__message" aria-expanded="false">
-        <div class="tooltip__label ${modifiers && modifiers.includes("tooltipShowText") ? "" : "tooltip__label--icon-only"}">
-          ${screenreaderText}
-        </div>
-        <div class="tooltip__tab"></div>
+	let tooltipHtml = `
+	<div class="tooltip">
+		<div id="${id}__label" aria-controls="${id}__message" class="tooltip__control" tabindex="0" role="button" aria-expanded="false">
+      <div class="tooltip__label ${modifiers && modifiers.includes("tooltipShowText") ? "" : "tooltip__label--icon-only"}">
+        <a>${screenreaderText}</a>
       </div>
-      <div id="${id}__message" class="tooltip__content" aria-labelledby="${id}__label" aria-hidden="true" role="region" tabindex="-1" >
-        <div class="tooltip__message">
-          ${content}
-        </div>
-        <div class="tooltip__close" role="button" tabindex="0">Close</div>
-      </div>
+      <div class="tooltip__tab"></div>
     </div>
-  `;
+  <div id="${id}__message" class="tooltip__content" aria-labelledby="${id}__label" role="region" tabindex="-1" aria-hidden="true">
+    <div class="tooltip__message">${content}
+      <div class="tooltip__close" role="button" tabindex="0">Close</div>
+    </div>
+  </div>
+	</div>`;
+
+  return tooltipHtml
 }
